ee8efcb13443a1da33171ac88b24bd7a6da4d62f
Ellesent <cool-francesca1@hotmail.com>
2016-01-30 12:15:28 -0700

    Main Menu with letter and name works

---
+++ TheCulling/TheCulling/Assets/Scripts/LetterClick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LetterClick : MonoBehaviour {
+    public Sprite RSVP;
+    public GameObject input;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().sprite = RSVP;
+        GetComponent<Collider2D>().enabled = false;
+       GameObject bob =  Instantiate(input) as GameObject;
+        bob.transform.SetParent(GameObject.Find("Canvas").transform);
+        bob.GetComponent<RectTransform>().anchoredPosition = new Vector2(-59, -39);
+
+    }
+}
