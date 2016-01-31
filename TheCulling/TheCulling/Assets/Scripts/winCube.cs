1a315a3aea336e1df66ad8fb2cba8ae7d451fd9f
and0ne808 <and0ne808@gmail.com>
2016-01-31 09:53:21 -0700

    added sheep, wolf, and human win scenes

---
+++ TheCulling/TheCulling/Assets/Scripts/winCube.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class winCube : MonoBehaviour {
+
+    public float activationDistance; //Recommendation = 10
+    public GameObject player;
+    private float currentDistance;
+    private bool win;
+
+	// Use this for initialization
+	void Start () {
+        win = false;
+	}
+	
+    void FixedUpdate()
+    {
+        currentDistance = Vector3.Distance(this.transform.position, player.transform.position);
+        if(currentDistance <= activationDistance )
+        {
+            win = true;
+            Debug.Log("You Win!");
+        }
+    }
+}
---
+++ TheCulling/TheCulling/Assets/Scripts/winZoneTry2.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var target : Collider; //this is the variable that will hold the colliding object
+private var triggered : boolean = false; //If we only want to detect the first time it's triggered
+
+    function OnTriggerEnter(collision : Collider)
+        {
+            if (collision != target) //The colliding object isn't our object
+            {
+                return; //don't do anything if it's not our target
+            }
+            triggered = true;
+            Debug.Log("triggered");
+        }
